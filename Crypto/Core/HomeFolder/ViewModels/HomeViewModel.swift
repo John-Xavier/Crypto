848702e2606902ec11f83ec02b29165c3b0cb770
John Xavier <94055839+John-Xavier@users.noreply.github.com>
2024-02-20 18:49:19 +0000

    Search functionality and filtering

--- Crypto/Core/HomeFolder/ViewModels/HomeViewModel.swift
+++ Crypto/Core/HomeFolder/ViewModels/HomeViewModel.swift
@@ -9,9 +9,11 @@ import Foundation
 import Combine
 
 class HomeViewModel : ObservableObject{
+    
     @Published var allCoins : [CoinModel] = []
     @Published var portfolioCoins : [CoinModel] = []
     @Published var searchText: String = ""
+    
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     init(){
@@ -19,10 +21,23 @@ class HomeViewModel : ObservableObject{
     }
     
     func addSubscribers(){
-        dataService.$allcoins
+        // this function updates all coins
+        $searchText
+            .combineLatest(dataService.$allcoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    private func filterCoins(text:String, coins:[CoinModel]) -> [CoinModel]{
+        guard !text.isEmpty else { return coins }
+        let lowercaseText = text.lowercased()
+        return coins.filter { coin in
+            return coin.name.lowercased().contains(lowercaseText) ||
+            coin.symbol.lowercased().contains(lowercaseText) ||
+            coin.id.lowercased().contains(lowercaseText)
+        }
     }
 }
